2de53b2bdd67edb04f35fa89cfb75aa48f79f1f4
Alexandr Leshko <sasha.leshko99@gmail.com>
2023-04-17 20:48:17 +0300

    total: 3 done

--- src/queue.js
+++ src/queue.js
@@ -1,34 +1,54 @@
 const { NotImplementedError } = require('../extensions/index.js');
+
 const { ListNode } = require('../extensions/list-node.js');
 
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+
 class Queue {
+
   constructor() {
     this.head = null;
     this.length = 0;
   }
+
   getUnderlyingList() {
-    let underlyingList;
-    (this.length === 0) ? underlyingList = new ListNode(null) : underlyingList = this.head;
-    return underlyingList;
-  }
-  enqueue(value) {
-    let newListNode = new ListNode(value);
+    let list;
     if (this.length === 0) {
-      this.head = newListNode;
+      list = new ListNode(null);
     } else {
-      let currentNode = this.head;
-      while (currentNode.next) {
-        currentNode = currentNode.next;
+      list = this.head;
+    }
+    return list;
+  }
+
+  enqueue( value ) {
+    let node = new ListNode(value);
+    if (this.length === 0) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
       }
-      currentNode.next = newListNode;
+      current.next = node;
     }
     this.length++;
   }
+
   dequeue() {
-    let deletedNode = this.head;
+    let del = this.head;
     this.head = this.head.next;
-    this.length--;
-    return deletedNode.value;
+    return del.value;
   }
 }
 

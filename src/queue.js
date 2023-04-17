4fada6836459846215e983fd48516a00d2d7b141
Alexandr Leshko <sasha.leshko99@gmail.com>
2023-04-17 21:28:50 +0300

    total: 3

--- src/queue.js
+++ src/queue.js
@@ -1,26 +1,11 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 const { ListNode } = require('../extensions/list-node.js');
 
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-
 class Queue {
-
   constructor() {
     this.head = null;
     this.length = 0;
   }
-
   getUnderlyingList() {
     let list;
     if (this.length === 0) {
@@ -30,7 +15,6 @@ class Queue {
     }
     return list;
   }
-
   enqueue( value ) {
     let node = new ListNode(value);
     if (this.length === 0) {
@@ -44,7 +28,6 @@ class Queue {
     }
     this.length++;
   }
-
   dequeue() {
     let del = this.head;
     this.head = this.head.next;
